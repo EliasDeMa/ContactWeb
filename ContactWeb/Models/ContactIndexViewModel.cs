dc3963e3e52d26377cdf4a4b13765f40c0273e1e
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-09 14:13:22 +0200

    Show list of current contacts.

---
+++ ContactWeb/Models/ContactIndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactWeb.Models
+{
+    public class ContactIndexViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
